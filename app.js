dc652add6ccb019a04dc6cc23879e024c1d1842e
gitTrap <ugo.trapuzzano@gmail.com>
2021-04-26 17:39:16 +0200

    modif responsive

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ function alertRedirect(event) {
 let ecran = document.querySelector(".ecran");
 let egal = document.querySelector(".egal");
 let ac = document.querySelector(".ac")
+let ec = document.querySelector(".ec")
     if (boutons != egal) {
         ecran.textContent += boutons.textContent;
+        
     }
     else {
      let calcul = eval(ecran.textContent);
      ecran.textContent = calcul
         }
     if (boutons != ac) {
-       ecran.textcontent = boutons.textContent;
+        boutons.textContent;
     }
     else {
         ecran.textContent = "";
     }
+
+  if (boutons != ec) {
+    boutons.textContent;
+  }
+
+  else {
+      ecran.textContent--
+  }
+
+
+    
 }
 let touches = document.querySelector(".touches");
 touches.addEventListener("click", alertRedirect);
-
 
 
 
